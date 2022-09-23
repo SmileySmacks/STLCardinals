8abff7134db1391df40de56e5336e69454cd53d1
Your Name <you@example.com>
2022-09-23 10:01:25 -0500

    created object.js file, added content

---
+++ Client/src/components/Object/object.js
@@ -0,0 +1,33 @@
+import { Button } from "@mui/material";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import "./object.css";
+const Object_function = (props) => {
+  const history = useNavigate();
+  const { _id, name, position, years, battingAvg, available } = props.object;
+  const deleteHandler = async () => {
+    await axios
+      .delete(`http://localhost:3001/cardinals/delete/${_id}`)
+      .then((res) => res.data)
+      .then(() => history("/"))
+      .then(() => history("/cardinals"));
+  };
+
+  return (
+    <div className="card">
+      <article> Name: {name}</article>
+      <h3> Position: {position}</h3>
+      <h3> Active: {years}</h3>
+      <h3> Batting Average: {battingAvg}</h3>
+      <h3> Availability: {available}</h3>
+      <Button LinkComponent={Link} to={`/cardinals/${_id}`} sx={{ mt: "auto" }}>
+            Update
+      </Button>
+      <Button color="error" onClick={deleteHandler} sx={{ mt: "auto" }}>
+        Delete Player
+      </Button>
+    </div>
+  );
+};
+
+export default Object_function;
